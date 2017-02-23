94ef98df486486eb974afdda780ef37431811391
Gulacsy-Toth <tompascall@gmail.com>
2017-02-23 21:44:51 +0100

    WIP: test babel-core calling

--- src/__tests__/main.spec.js
+++ src/__tests__/main.spec.js
@@ -1,5 +1,6 @@
-import { process } from '../main';
+import { process, transformer } from '../main';
 import * as riot from 'riot';
+import { transform } from 'babel-core';
 
 const hello = `
     <hello>
@@ -10,12 +11,29 @@ const hello = `
 describe('riot-jest-transformer', function() {
 
     it('should be a function', function() {
-     expect(typeof process).toBe('function');  
-    }); 
+     expect(typeof process).toBe('function');
+    });
 
     it('gives back the compiled tag', function() {
-        console.log(process(hello).search(/riot.tag2\(['|"]hello['|"]/));
         expect(process(hello).search(/riot.tag2\(['|"]hello['|"]/)).not.toEqual(-1);
     });
+
+  it('should call getTransformed of transformer', () => {
+    spyOn(transformer, 'getTransformed').and.callThrough();
+    process(hello);
+    expect(transformer.getTransformed).toHaveBeenCalled();
+  });
+
+  it('should transform the compiled tag with babel-core', () => {
+    const transformed = transform(riot.compile(hello));
+    console.log('transformed', transformed)
+    expect(transformed.code).toEqual(process(hello));
+  });
+
+  xit('provides its second parameter as filename for babel-core', () => {
+  });
+
+  xit('should provide riot module in order to be able to run when imported', () => {
+  });
 });
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 const riot = require('riot');
+const { transform } = require('babel-core');
+
+const transformer = {
+  getCompiled (source) {
+    return riot.compile(source);
+  },
+
+  getTransformed (compiled) {
+    return transform(compiled);
+  }
+}
 
 exports.process = function (source) {
-  return riot.compile(source);
+  let compiled = transformer.getCompiled(source);
+  let transformed = transformer.getTransformed(compiled);
+  console.log('transformed', transformed);
+  return transformed.code;
 }
+
+exports.transformer = transformer;
