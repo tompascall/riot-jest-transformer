a2aede2aa5ac5489d178501160b8afead4cb8dbd
Gulacsy-Toth <tompascall@gmail.com>
2017-02-25 16:06:26 +0100

    make process abel to get other transformers than babel-core

--- src/__tests__/main.spec.js
+++ src/__tests__/main.spec.js
@@ -1,6 +1,8 @@
 import { process, transformer } from '../main';
 import * as riot from 'riot';
 import { transform } from 'babel-core';
+import path from 'path';
+import fs from 'fs';
 
 const hello = `
     <hello>
@@ -8,32 +10,92 @@ const hello = `
     </hello>
 `;
 
+const configPath = `${path.resolve('.riot-jest-tranformer')}`;
+
 describe('riot-jest-transformer', function() {
 
+  describe('getDefaultConfig', () => {
+    it('should use babel transformer optioned with filename', () => {
+      const config = transformer.getDefaultConfig({ filename: 'fakeFile'});
+      expect(config.transformer).toEqual('babel-core');
+      expect(config.method).toEqual('transform');
+      expect(config.args).toEqual([{ filename: 'fakeFile' }]);
+    });
+  });
+
+  describe('getConfig', () => {
+    it('should use babel-core as default transformer if no config file', () => {
+      const callGetConfig = function () {
+        transformer.getConfig()
+      };
+
+      if (fs.existsSync(configPath)) {
+        fs.unlinkSync(configPath);
+        expect(callGetConfig).not.toThrow();
+        const config = transformer.getConfig({ filename: 'fakeFile' });
+        expect(config).toEqual(transformer.getDefaultConfig({ filename: 'fakeFile' }));
+      }
+    });
+
+    it('should use .riot-jest-transformer file as config if it exists', () => {
+      let config = {
+        transformer: "babel-core",
+        method: 'transform',
+        args: [{filename: 'fakeFile'}, 'fakeArg']
+      };
+
+      fs.writeFileSync(configPath, JSON.stringify(config), {encoding: 'utf8'});
+      expect(transformer.getConfig({ filename: 'fakeFile'})).toEqual(config);
+    });
+  });
+
+  describe('getTransformed', () => {
+
+    it('uses babel-core transform if config does not exist', () => {
+      if (fs.existsSync(configPath)) {
+        fs.unlinkSync(configPath);
+      }
+
+      let compiled = transformer.getCompiled(hello);
+      let result = transformer.getTransformed({ compiled, args: [{filename: 'fakeFile'}] });
+      expect(result).toEqual(transform(compiled, {filename: 'fakeFile'}));
+    });
+
+    it('should use the provided transformer, method and args if given', () => {
+      let compiled = transformer.getCompiled(hello);
+      let fakeNodeModule = `
+      exports.fakeFormer = function (compiled, options) {
+          return 'fake_' + options.lalala + '____' + compiled;
+      };
+      `
+      fs.writeFileSync(path.resolve('node_modules/fakeNodeModule.js'), fakeNodeModule, {encoding: 'utf8'});
+      const result = transformer.getTransformed({
+        compiled,
+        transformer: 'fakeNodeModule',
+        method: 'fakeFormer',
+        args: [{ lalala: 'lalala' }]
+      });
+      expect(result.split('____')[0]).toEqual('fake_lalala');
+      expect(result.split('____')[1]).toEqual(compiled);
+      fs.unlinkSync(path.resolve('node_modules/fakeNodeModule.js'));
+    });
+  });
+
+  describe('process', () => {
     it('should be a function', function() {
-     expect(typeof process).toBe('function');
+        expect(typeof process).toBe('function');
     });
 
     it('gives back the compiled tag', function() {
         expect(process(hello).search(/riot.tag2\(['|"]hello['|"]/)).not.toEqual(-1);
     });
 
-  it('should call getTransformed of transformer', () => {
-    spyOn(transformer, 'getTransformed').and.callThrough();
-    process(hello);
-    expect(transformer.getTransformed).toHaveBeenCalled();
+    it('should call getConfig with the second argument (provided by jest as filename)', () => {
+      spyOn(transformer, 'getConfig');
+      process(hello, 'fakeFile');
+      expect(transformer.getConfig).toHaveBeenCalledWith({ filename: 'fakeFile'});
+    });
   });
 
-  it('should transform the compiled tag with babel-core', () => {
-    const transformed = transform(riot.compile(hello));
-    console.log('transformed', transformed)
-    expect(transformed.code).toEqual(process(hello));
-  });
-
-  xit('provides its second parameter as filename for babel-core', () => {
-  });
-
-  xit('should provide riot module in order to be able to run when imported', () => {
-  });
 });
 
--- src/main.js
+++ src/main.js
@@ -1,20 +1,46 @@
 const riot = require('riot');
 const { transform } = require('babel-core');
-
+const path = require('path');
+const fs = require('fs');
+const CONFIG_PATH = `${path.resolve('.riot-jest-tranformer')}`;
+//console.log(CONFIG_PATH, 'XXX'.repeat(50));
 const transformer = {
   getCompiled (source) {
     return riot.compile(source);
   },
 
-  getTransformed (compiled) {
-    return transform(compiled);
+  getDefaultConfig ({ filename } = {}) {
+    return {
+      transformer: 'babel-core',
+      method: 'transform',
+      args: [{
+        filename
+      }]
+    }
+  },
+
+  getConfig ({ filename = '' } = {}) {
+    let config;
+    if (fs.existsSync(CONFIG_PATH)) {
+      return JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf8'))
+    }
+    else return this.getDefaultConfig({ filename });
+  },
+
+  getTransformed ({ compiled, transformer, method, args = [] } = {}) {
+    args.unshift(compiled);
+    if (transformer && method) {
+      const transformerByParam = require(transformer);
+      return transformerByParam[method](...args);
+    }
+    return transform(...args);
   }
 }
 
-exports.process = function (source) {
+exports.process = function (source, filename) {
   let compiled = transformer.getCompiled(source);
-  let transformed = transformer.getTransformed(compiled);
-  console.log('transformed', transformed);
+  const config = transformer.getConfig({ filename });
+  let transformed = transformer.getTransformed({ compiled, ...config });
   return transformed.code;
 }
 
