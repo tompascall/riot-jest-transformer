6882831750721d4cead360380d0c2086eaa6d2b4
Sam Brenner <samjbrenner@gmail.com>
2019-02-07 12:19:50 -0500

    upgrade babel 6 -> 7

--- demo/jest.config.js
+++ demo/jest.config.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
     "verbose": true,
     "automock": false,
     "transform": {
-      "^.+\\.jsx?$": "babel-jest",
-      "^.+\\.tag$": "../src/main.js"
+        "^.+\\.jsx?$": "babel-jest",
+        "^.+\\.tag$": "../src/main.js"
     }
-}
+};
