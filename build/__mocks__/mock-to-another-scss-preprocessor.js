a6a652bd569db4d73a42f396152a55887d2baf3c
Gulacsy-Toth <tompascall@gmail.com>
2019-10-05 16:34:22 +0200

    add build

---
+++ build/__mocks__/mock-to-another-scss-preprocessor.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function mockScssPreprocessor() { }
+exports.default = mockScssPreprocessor;
+;
+//# sourceMappingURL=mock-to-another-scss-preprocessor.js.map
