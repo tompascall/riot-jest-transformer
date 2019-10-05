1bec9191fe2427c74f2922d2f93dd2443aa15159
Gulacsy-Toth <tompascall@gmail.com>
2019-10-05 15:58:36 +0200

    add npm scripts to clean the build

--- build/__mocks__/mock-to-another-scss-preprocessor.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function mockScssPreprocessor() { }
-exports.default = mockScssPreprocessor;
-;
-//# sourceMappingURL=mock-to-another-scss-preprocessor.js.map
