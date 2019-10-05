a564b40db95150915745b32ada991a38fa6bfece
Gulacsy-Toth <tompascall@gmail.com>
2019-10-05 17:09:27 +0200

    remove tests and mocks from build

--- build/__mocks__/mock-scss-preprocessor.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function mockScssPreprocessor() { }
-exports.default = mockScssPreprocessor;
-;
-//# sourceMappingURL=mock-scss-preprocessor.js.map
